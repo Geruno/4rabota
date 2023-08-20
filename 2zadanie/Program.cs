1051761f6e27e5b61a21c1bdd06d2b99acc1b798
Geruno <8vampir8@gmail.com>
2023-08-20 11:06:01 +0700

    Сделал 2 задание

--- 2zadanie/Program.cs
+++ 2zadanie/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+string str_num = Console.ReadLine();
+int num = int.Parse(str_num);
+int Length(int num)
+{
+    int rez = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        rez++;
+    }
+    return rez;
+}
+int leng = Length(num);
+int Summ(int num, int leng)
+{
+    int summ = 0;
+    for (int i=0; i <= leng; i++)
+    {
+        summ += num % 10;
+        num /= 10;
+    }
+    return summ;
+}
+Console.WriteLine($"Сумма цифр числа {num} равна {Summ(num,leng)}");
