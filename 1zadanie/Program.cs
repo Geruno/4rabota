9efded8d7f1529d3d32ba6f4b8b372040b5c68fa
Geruno <8vampir8@gmail.com>
2023-08-20 10:39:31 +0700

    Сделал 1 задание

--- 1zadanie/Program.cs
+++ 1zadanie/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+string str_num_1 = Console.ReadLine();
+int num_1 = int.Parse(str_num_1);
+Console.WriteLine("Введите второе число: ");
+string str_num_2 = Console.ReadLine();
+int num_2 = int.Parse(str_num_2);
+int Vozvedenie(int num_1, int num_2)
+{
+    int rez = 1;
+    for(int i=1; i<=num_2; i++)
+    {
+        rez = rez * num_1;
+    }
+    return rez;
+}
+int vozved = Vozvedenie(num_1, num_2);
+Console.WriteLine($"{vozved}");
