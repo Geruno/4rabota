f69221b898305c1b98ac089b55984a0dd98c01e1
Geruno <8vampir8@gmail.com>
2023-08-20 11:13:58 +0700

    Сделал 3 задание

--- 3zadanie/Program.cs
+++ 3zadanie/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] random_array = new int[8];
+Console.Write("[");
+for (int i = 0; i < 8; i++)
+{
+    random_array[i] = new Random().Next(1,99);
+    if (i<7)
+    Console.Write($"{random_array[i]},");
+    else
+    Console.Write($"{random_array[i]}");
+}
+Console.Write("]");
--- 3zadanie/obj/Debug/net8.0/3zadanie.AssemblyInfo.cs
+++ 3zadanie/obj/Debug/net8.0/3zadanie.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("2zadanie")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("3zadanie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1051761f6e27e5b61a21c1bdd06d2b99acc1b798")]
-[assembly: System.Reflection.AssemblyProductAttribute("2zadanie")]
-[assembly: System.Reflection.AssemblyTitleAttribute("2zadanie")]
+[assembly: System.Reflection.AssemblyProductAttribute("3zadanie")]
+[assembly: System.Reflection.AssemblyTitleAttribute("3zadanie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
